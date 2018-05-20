98cc0bf2bf5a29158df9c670a414d0f6c58d01c6
chenchangan6 <chenchangan6@gmail.com>
2018-05-20 22:50:17 +0800

    增加了element-ui插件

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,31 +3,31 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "pmpweb1000",
+    title: 'pmpweb1000',
     meta: [{
-        charset: "utf-8"
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      },
-      {
-        hid: "description",
-        name: "description",
-        content: "Nuxt.js project"
-      }
+      charset: 'utf-8'
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Nuxt.js project'
+    }
     ],
     link: [{
-      rel: "icon",
-      type: "image/x-icon",
-      href: "/favicon.ico"
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
     }]
   },
   /*
    ** Customize the progress bar color
    */
   loading: {
-    color: "#3B8070"
+    color: '#3B8070'
   },
   /*
    ** Build configuration
@@ -42,12 +42,35 @@ module.exports = {
     }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
-        });
+        })
       }
     }
-  }
-};
+  },
+  vender: [
+    'element-ui'
+  ],
+  babel: {
+    'plugins': [
+      ['component', [{
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-default'
+      },
+      'transform-async-to-generator',
+      'transform-runtime'
+      ]]
+    ],
+    comments: true
+  },
+  plugins: [{
+    src: '~plugins/element-ui',
+    ssr: true
+  }],
+  css: [
+    // 全部引用的时候需要用到
+    'element-ui/lib/theme-chalk/index.css'
+  ]
+}
