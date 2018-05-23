3e4767ff16b961e0cbeee7172e7a7cf3a1e77204
chenchangan6 <chenchangan6@gmail.com>
2018-05-23 19:00:15 +0800

    增加后台登录模板

---
+++ layouts/backstage/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <nuxt-link to="/"
+               class="shangfu">返回根目录</nuxt-link>
+    
+    <nuxt/>
+<!-- 这是后台登录模板 -->
+  </div>
+</template>
+<style>
+html,
+body,
+#__nuxt,
+#__layout,
+#app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.shangfu {
+  font-size: 12px;
+}
+</style>
+
